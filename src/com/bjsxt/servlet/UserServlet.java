b03f7dfc8afd52e69474b713643135cdf2a125b1
xj <1321052920@qq.com>
2020-03-11 17:03:27 +0800

    l

---
+++ src/com/bjsxt/servlet/UserServlet.java
@@ -0,0 +1,15 @@
+package com.bjsxt.servlet;
+
+
+import javax.servlet.http.HttpServlet;
+
+public class UserServlet extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public UserServlet() {
+		super();
+	}
+
+}
