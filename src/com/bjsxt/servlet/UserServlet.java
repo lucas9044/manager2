036c83c5039f3651cde644d45c9dc91030f42790
xj <1321052920@qq.com>
2020-03-13 17:40:37 +0800

    kk

--- src/com/bjsxt/servlet/UserServlet.java
+++ src/com/bjsxt/servlet/UserServlet.java
@@ -1,15 +1,35 @@
 package com.bjsxt.servlet;
 
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class UserServlet extends HttpServlet {
-
-	/**
-	 * Constructor of the object.
-	 */
-	public UserServlet() {
-		super();
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		//设置请求编码格式
+		req.setCharacterEncoding("utf-8");
+		//设置相应编码格式
+		resp.setContentType("text/html;charset=utf-8");
+		//调用登录处理方法
+		checkUserLogin(req,resp);
+		//获取请求信息
+		//处理请求信息
+		//响应处理结果
+			//直接响应
+			//请求转发
+			//重定向
 	}
-
+	//处理登录
+	private void checkUserLogin(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
